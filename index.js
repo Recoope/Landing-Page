e8bc3adb21842f3f0ef53e643568cb75d336e3fa
Nicolas Nicodem <nicodemnicolas@gmail.com>
2024-10-25 12:57:59 -0300

    Subindo o login da area restrita

--- index.js
+++ index.js
@@ -45,3 +45,38 @@ toggleButton.addEventListener("click", () => {
     }
 });
 
+function login(emailInput, passwordInput) {
+    const baseUrl = 'http://ec2-44-194-250-226.compute-1.amazonaws.com:8080';
+    const url = baseUrl + '/login';
+
+    const data = {
+        cnpjOuEmail: emailInput.value,
+        senha: passwordInput.value
+    };
+
+    console.log('Dados a serem enviados:', data);
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => {
+        console.log('Status da resposta:', response.status);
+        if (response.status === 200) {
+            return response.json();
+        } else {
+            throw new Error(`Erro na requisição: ${response.status}`);
+        }
+    })
+    .then(data => {
+        console.log('Dados recebidos:', data);
+        window.location.href = 'home.html';
+    })
+    .catch(error => {
+        console.error('Houve um problema com a requisição:', error);
+        window.alert("Credenciais incorretas")
+    });
+}
